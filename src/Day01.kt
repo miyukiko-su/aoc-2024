d5cfdaafcb00f2c80735f734d7d2c0e8b927b993
Kasperunas, I. <morroni5x@gmail.com>
2024-12-01 23:23:11 +0900

    Add task solution for Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,71 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun runPart(partId: String, executor: (List<String>) -> Int) {
+        execute(
+            executor = executor,
+            testDataPath = "Day01_test-data.txt",
+            testResultPath = "Day01_test-result_${partId}.txt",
+            realDataPath = "Day01_real-data.txt"
+        )
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    runPart("part1", ::calculateDifferenceScore)
+    runPart("part2", ::calculateSimilarityScore)
+}
+
+fun execute(executor: (List<String>) -> Int, testDataPath: String, testResultPath: String, realDataPath: String) {
+    val testInput = readInput(testDataPath)
+    val testResult = executor(testInput)
+
+    val expectedResult = readInput(testResultPath).first().toInt()
+    "Expected result for the test data: $expectedResult".println()
+    "Received result for the test data: $testResult".println()
+    check(testResult == expectedResult)
+
+    val realInput = readInput(realDataPath)
+    val result = executor(realInput)
+    "Result for the actual data: $result".println()
+}
+
+fun calculateDifferenceScore(inputLines: List<String>): Int {
+    val group1 = mutableListOf<Int>()
+    val group2 = mutableListOf<Int>()
+
+    inputLines.forEach { line ->
+        val (number1, number2) = line.split("   ").map { number -> number.toInt() }
+        group1.insertWithAscendingOrder(number1)
+        group2.insertWithAscendingOrder(number2)
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    if (group1.size != group2.size) {
+        return 0
+    }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    var sum = 0
+    for (i in 0..<group1.size) {
+        val diff = group1[i] - group2[i]
+        sum = when (diff < 0) {
+            true -> sum - diff
+            else -> sum + diff
+        }
+    }
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    return sum
+}
+
+fun calculateSimilarityScore(inputLines: List<String>): Int {
+    val group1 = mutableListOf<Int>()
+    val group2ElementsOccurrence = mutableMapOf<Int, Int>()
+
+    inputLines.forEach { line ->
+        val (number1, number2) = line.split("   ").map { number -> number.toInt() }
+        group1.insertWithAscendingOrder(number1)
+        group2ElementsOccurrence.compute(number2) { _, v -> if (v == null) 1 else v + 1 }
+    }
+
+    var sum = 0
+    for (number in group1) {
+        sum += number * group2ElementsOccurrence.getOrDefault(number, 0)
+    }
+
+    return sum
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readText
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+fun readInput(name: String) = Path("src/$name").readText().trim().lines()
 
 /**
  * Converts string to md5 hash.
@@ -19,3 +19,21 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun MutableList<Int>.insertWithAscendingOrder(newElement: Int) {
+    if (this.size == 0) {
+        this.add(newElement)
+        return
+    }
+
+    for (i in 0..<this.size) {
+        if (this[i] < newElement) {
+            continue
+        }
+
+        this.add(i, newElement)
+        return
+    }
+
+    this.add(newElement)
+}
